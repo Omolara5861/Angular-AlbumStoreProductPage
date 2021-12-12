2c00bf01c43eec59e101ab26e5e443dfa7cb963f
Lara <debbiegterra@gmail.com>
2021-12-12 14:00:09 +0100

    module 6 section

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { Product } from '../product';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  products : Product[];
+
+  constructor(private _productService) { }
+
+  ngOnInit() {
+    this._productService.getProduct().subscribe( response => 
+      this.products = response);
+  }
+}
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,17 +3,24 @@ import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs'; 
 import  'rxjs/add/operator/map';
 import { Album } from "./album";
+import { Product } from './product'
 
 @Injectable()
 export class ProductService {
 
     private _albumUrl: string = '../assets/album.json';
+    private productUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map(response => 
       <Album>response.json());
+  }
+
+  getProduct(): Observable<Product[]> {
+    return this._http.get(this.productUrl).map(response => 
+      <Product[]>response.json());
   }
 
 }
